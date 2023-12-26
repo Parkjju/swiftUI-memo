f939eab8a4d2f9b42494b27217270f52743b1e3b
Parkjju <rudwns3927@gmail.com>
2023-12-26 15:16:05 +0900

    chore: 프로젝트 초기 세팅

---
+++ swiftUIMemo/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListView.swift
+//  swiftUIMemo
+//
+//  Created by 박경준 on 12/26/23.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
--- swiftUIMemo/swiftUIMemoApp.swift
+++ swiftUIMemo/swiftUIMemoApp.swift
@@ -13,8 +13,7 @@ struct swiftUIMemoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            MainListView()
         }
     }
 }
